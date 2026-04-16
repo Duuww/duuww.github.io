ebf453137105393fcc319c929c6386385a828f14
ww <1539689479@qq.com>
2026-04-16 10:45:37 +0800

    更新linkedin链接

--- assets/js/app.js
+++ assets/js/app.js
@@ -163,7 +163,7 @@ let footer = $(`
 
     <div class="rounded-social-buttons tag">
 
-    <a class="social-button linkedin" href="" target="_blank" aria-label="go to linkedin">
+    <a class="social-button linkedin" href="https://www.linkedin.com/in/%E4%B8%BA-%E6%B1%AA-343a53403/" target="_blank" aria-label="go to linkedin">
     <!-- svg code for linkedin icon -->
       <svg class="linkedin-icon-footer" xmlns="http://www.w3.org/2000/svg" width="35" viewBox="0 0 24 24" fill="#0e76a8"
         stroke-width="1" stroke-linecap="round" stroke-linejoin="round" class="feather feather-linkedin">
